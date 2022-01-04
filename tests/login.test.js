1cb281900ce3fe390ef23817584ebcf24d1debfa
luciagonzalez <lucia.gonzalez@abstracta.com.uy>
2022-01-04 17:30:05 -0300

    add const VALID_USERNAME and VALID_PASSWORD

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,6 +1,9 @@
 import LOGIN_PAGE from '../pages/login.page';
 const LOGIN_PAGE = LOGIN_PAGE;
 
+const VALID_USERNAME = 'jsmith@demo.io'
+const VALID_PASSWORD = 'Demo123!'
+
 
 describe('Login Test Cases', () => {
   beforeEach(async function  () {
@@ -13,21 +16,21 @@ describe('Login Test Cases', () => {
     });
 
     it('Should log into demo account when valid data is entered', async () => {
-      await LOGIN_PAGE.logInWithSubmitButton('jsmith@demo.io', 'Demo123!');
+      await LOGIN_PAGE.logInWithSubmitButton(VALID_USERNAME, VALID_PASSWORD);
 
       expect(browser).toHaveUrlContaining('home');
 
     });
 
     it('Should log into demo account when using enter key', async () => {
-      await LOGIN_PAGE.logInWithEnter('jsmith@demo.io', 'Demo123!');
+      await LOGIN_PAGE.logInWithEnter(VALID_USERNAME, VALID_PASSWORD);
 
       expect(browser).toHaveUrlContaining('home');
 
     });
 
     it('Should deny access when invalid data is entered', async () => {
-      await LOGIN_PAGE.logInWithSubmitButton('luciagonzalez@demo.io', 'Demo123!');
+      await LOGIN_PAGE.logInWithSubmitButton('luciagonzalez@demo.io', VALID_PASSWORD);
 
       expect(LOGIN_PAGE.failedLoginMessage).toHaveText('Error');
        
@@ -40,7 +43,7 @@ describe('Login Test Cases', () => {
     });
 
     it('Should remember the inputs when the button is checked', async () => {
-      await LOGIN_PAGE.logInWithRememberMeChecked('jsmith@demo.io', 'Demo123!');
+      await LOGIN_PAGE.logInWithRememberMeChecked(VALID_USERNAME, VALID_PASSWORD);
       await browser.newWindow('login');
   
       expect(LOGIN_PAGE.usernameInput).toHaveText('jsmith@demo.io');
--- tests/logout.test.js
+++ tests/logout.test.js
@@ -6,6 +6,10 @@ import BasePage from '../pages/base.page';
 const LOGIN_PAGE = LoginPage;
 const BASE_PAGE = BasePage;
 
+const VALID_USERNAME = 'jsmith@demo.io';
+const ANOTHER_VALID_USERNAME = 'nsmith@demo.io';
+const VALID_PASSWORD = 'Demo123!';
+
 
 describe('Logout Test Cases', () => {
   beforeEach(async function  () {
@@ -13,7 +17,7 @@ describe('Logout Test Cases', () => {
   });
 
   it('Should logout successful when click on the Logout button', async () => {
-    await LOGIN_PAGE.logInWithSubmitButton('jsmith@demo.io', 'Demo123!');
+    await LOGIN_PAGE.logInWithSubmitButton(VALID_USERNAME, VALID_PASSWORD);
     await BASE_PAGE.clickElement(LogoutPage.profileIcon);
     await BASE_PAGE.clickElement(LogoutPage.logoutButton);
 
@@ -21,15 +25,12 @@ describe('Logout Test Cases', () => {
   });
   
   it('Should close a session when you try to have 2 simultaneously', async () => {
-    await LOGIN_PAGE.logInWithSubmitButton('jsmith@demo.io', 'Demo123!');
-
+    await LOGIN_PAGE.logInWithSubmitButton(VALID_USERNAME, VALID_PASSWORD);
     await browser.newWindow('login');
-    await LOGIN_PAGE.logInWithSubmitButton('nsmith@demo.io', 'Demo123!');
-    await LOGIN_PAGE.logInWithSubmitButton();
-
-    
+    await LOGIN_PAGE.logInWithSubmitButton(ANOTHER_VALID_USERNAME, VALID_PASSWORD);    
     browser.switchWindow('home');
     await browser.refresh();
+
     expect(HomePage.welcomeMsg).toHaveText('Welcome Nicole');
   });
 
