67a5842a86d91870e4303ca0c1d5937e71bd9711
luciagonzalez <lucia.gonzalez@abstracta.com.uy>
2022-01-04 14:34:06 -0300

    modify welcomeMsg selector

--- pages/home.page.js
+++ pages/home.page.js
@@ -2,7 +2,7 @@ const PAGE_TIMEOUT = 10000;
 
 class HomePage {
        
-        get welcomeMsg () { return $('[class="active"]'); } // implementar cambios
+        get welcomeMsg () { return $('[class="page-title"]'); }
 
 }
 export default new HomePage();
